7a826e5e57ce793ca5a926f2857a92c72b1ed2fc
Elan Tamez <tamezapple@gmail.com>
2025-10-21 13:29:54 -0600

    Initial Commit

---
+++ MAP KIT/MAP_KITApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAP_KITApp.swift
+//  MAP KIT
+//
+//  Created by Tamez, Elan - Student on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MAP_KITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MAP KITTests/MAP_KITTests.swift
@@ -0,0 +1,17 @@
+//
+//  MAP_KITTests.swift
+//  MAP KITTests
+//
+//  Created by Tamez, Elan - Student on 10/21/25.
+//
+
+import Testing
+@testable import MAP_KIT
+
+struct MAP_KITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
