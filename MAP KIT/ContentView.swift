db30b0380d3422e6f6dc1cae8bb2d8541f99b192
Elan Tamez <tamezapple@gmail.com>
2025-10-30 13:59:51 -0600

    hjhhjj

--- MAP KIT/ContentView.swift
+++ MAP KIT/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     
     let location1 = CLLocationCoordinate2D(latitude: 34.0549, longitude: -118.2426)
-    let location2 = CLLocationCoordinate2D(latitude: 33.7701, longitude: -118.1937)
+    let location2 = CLLocationCoordinate2D(latitude: 33.9850, longitude: -118.4743)
     let location3 = CLLocationCoordinate2D(latitude: 34.0545, longitude: -118.2406)
     let location4 = CLLocationCoordinate2D(latitude: 34.4249, longitude: -118.5958)
     let location5 = CLLocationCoordinate2D(latitude: 34.0985, longitude: -118.3256)
@@ -23,18 +23,18 @@ struct ContentView: View {
     
     var body: some View {
         Map(position: $camera) {
-            Marker("Mystery location", coordinate: location1)
+            Marker("Los angeles City center", coordinate: location1)
                 .tint(.black)
                
             
             
-            Marker("Mystery location", coordinate: location2)
+            Marker("Ocean", coordinate: location2)
                 .tint(.black)
-            Marker("Mystery location", coordinate: location3)
+            Marker("mall", coordinate: location3)
                 .tint(.black)
-            Marker("Mystery location", coordinate: location4)
+            Marker("6 flags", coordinate: location4)
                 .tint(.black)
-            Marker("Mystery location", coordinate: location5)
+            Marker("Stars", coordinate: location5)
                 .tint(.black)
             
         }
@@ -44,7 +44,7 @@ struct ContentView: View {
                     camera = .region(MKCoordinateRegion(center: location1, latitudinalMeters:300, longitudinalMeters:300))
                 } label:  {
                     Text("Los angeles")
-                        .background(.black)
+                        .background(.blue)
                         .foregroundStyle(.white)
                    
                 }
@@ -52,28 +52,28 @@ struct ContentView: View {
                     camera = .region(MKCoordinateRegion(center: location2, latitudinalMeters:300, longitudinalMeters:300))
                 } label: {
                     Text("Ocean")
-                        .background(.black)
+                        .background(.blue)
                         .foregroundStyle(.white)
                 }
                 Button {
                     camera = .region(MKCoordinateRegion(center: location3, latitudinalMeters:300, longitudinalMeters:300))
                 } label: {
                     Text("mall")
-                        .background(.black)
+                        .background(.blue)
                         .foregroundStyle(.white)
                 }
                 Button {
                     camera = .region(MKCoordinateRegion(center: location4, latitudinalMeters:300, longitudinalMeters:300))
                 } label: {
                     Text("6 flags")
-                        .background(.black)
+                        .background(.blue)
                         .foregroundStyle(.white)
                 }
                 Button {
                     camera = .region(MKCoordinateRegion(center: location5, latitudinalMeters:300, longitudinalMeters:300))
                 } label: {
                     Text("Stars")
-                        .background(.black)
+                        .background(.blue)
                         
                         .foregroundStyle(.white)
                         
